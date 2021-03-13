b6fa06d441f62932f170eee9564568132f45772b
dastronmighty <eoghanthehogan@gmail.com>
2021-03-13 23:06:49 +0000

    test model on one batch funciton added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,17 @@
+from src.Data.Data import Data
+from src.utils.Metrics import auc, acc
+
 from matplotlib import pyplot as plt
 from datetime import datetime
-from torchvision import transforms
+from tqdm import tqdm
+
 import shutil
 import os
 
+import numpy as np
+
+import torch
+from torchvision import transforms
 
 def curr_time():
     ct = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -55,4 +63,37 @@ def resize_wrapper(x, y, s):
 
 def get_resize_wrapper(size):
     return lambda x, y: resize_wrapper(x, y, size)
+
+
+def test_model_on_one_batch(epochs, model, m_kwargs, p, wrapped):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using {device}")
+    print("Loading Data...")
+    data = Data(p, wrapped_function=wrapped, device=device)
+    x, y = next(iter(data.get_train_data()))
+    mod = model(**m_kwargs)
+    mod = mod.to(device)
+    opt = torch.optim.Adam(params=mod.parameters(), lr=0.00025)
+    loss_func = torch.nn.BCELoss()
+    mets = {
+        "loss": [],
+        "acc": [],
+        "auc": []
+    }
+    for i in tqdm(range(epochs), "epoch"):
+        y_hat = mod(x)
+        y_hat = y_hat.flatten()
+        loss = loss_func(y_hat, y.float())
+        mets["loss"].append(loss.item())
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+        mets["auc"].append(acc(y.detach().cpu().numpy(), y_hat.detach().cpu().numpy()))
+        mets["acc"].append(auc(y.detach().cpu().numpy(), y_hat.detach().cpu().numpy()))
+        print(f"Loss = {mets['loss'][-1]} - acc = {mets['acc'][-1]} - auc = {mets['auc'][-1]}")
+    print("")
+    print(f"Min loss reached {np.min(mets['loss'])} - reach on epoch {np.argmin(mets['loss'])}")
+    print(f"Max accuracy reached {np.max(mets['acc'])} - reach on epoch {np.argmax(mets['acc'])}")
+    print(f"Max AUC reached {np.max(mets['auc'])} - reach on epoch {np.argmax(mets['auc'])}")
+
 
